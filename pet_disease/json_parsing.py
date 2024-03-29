d65cb4570b5907c539abb2fc4bdcd6acaf608ee7
yurim <117262012+YuRimBang@users.noreply.github.com>
2024-03-29 17:45:19 +0900

    recommend

--- pet_disease/json_parsing.py
+++ pet_disease/json_parsing.py
@@ -2,7 +2,7 @@ import json
 import os
 import csv
 
-def data_paring(path):
+def data_paring(path, pet):
     # 파일 열기
     with open(path, encoding='UTF8') as file:
         # JSON 데이터 로드
@@ -11,23 +11,40 @@ def data_paring(path):
 
         # 각 데이터 파싱
         type = meta['type']
+
         species, mission_id, breed, age, pet_class, sex = id_data_paring(meta)
-        weight = physical_data_paring(meta)
-        exercise, environment, defecation, food_count, food_amount, snack_amount, food_kind = breeding_data_paring(meta)
         disease, disease_name, CRP, IgG, IL_6, AFP = medical_data_paring(meta)
+        weight = None
+        exercise = None
+        environment = None
+        defecation = None
+        food_count = None
+        food_amount = None
+        snack_amount = None
+        food_kind = None
+        stress = None
+
+        if(type == 'B'):
+            weight = physical_data_paring(meta)
+            exercise, environment, defecation, food_count, food_amount, snack_amount, food_kind = breeding_data_paring(meta)
+            stress = vital_data_paring(meta)
 
         # CSV에 쓸 데이터 리스트로 구성
         pet_data = [species, mission_id, breed, age, pet_class, sex, weight, exercise,
                     environment, defecation, food_count, food_amount, snack_amount, food_kind,
-                    disease, disease_name, CRP, IgG, AFP]
+                    disease, disease_name, CRP, IgG, AFP, stress]
 
         print(path)
         print(pet_data)
 
-        # CSV 파일에 데이터 쓰기
-        with open('data/dog_data.csv', 'a', newline='', encoding='UTF8') as f:
-            csv_writer = csv.writer(f)
-            csv_writer.writerow(pet_data)
+        if pet == "반려견":
+            with open('data/dog_data.csv', 'a', newline='', encoding='UTF8') as f:
+                csv_writer = csv.writer(f)
+                csv_writer.writerow(pet_data)
+        elif pet == "반려묘":
+            with open('data/cat_data.csv', 'a', newline='', encoding='UTF8') as f:
+                csv_writer = csv.writer(f)
+                csv_writer.writerow(pet_data)
 
 def id_data_paring(meta):
     # 기본 정보 파싱
@@ -63,27 +80,34 @@ def medical_data_paring(meta):
     medical = meta['medical']
     disease = medical['disease']
     disease_name = None
-    CRP = None
-    IgG = None
-    IL_6 = None
-    AFP = None
+
     if disease == 'ABN':
         disease_name = medical['diseaseName']
-        CRP = medical['CRP']
-        IgG = medical['lgG']
-        IL_6 = medical['IL-6']
-        AFP = medical['AFP']
+
+    CRP = medical['CRP']
+    IgG = medical['lgG']
+    IL_6 = medical['IL-6']
+    AFP = medical['AFP']
+
     return disease, disease_name, CRP, IgG, IL_6, AFP
 
+def vital_data_paring(meta):
+    vital = meta['vital']
+    stress = vital['stress']
+
+    return stress
+
 def process_directory(directory):
-    # 디렉토리 안의 모든 파일 순회
     for root, dirs, files in os.walk(directory):
         for file in files:
-       # if file.endswith(".json"):
             file_path = os.path.join(root, file)
-            #print(file_path)
-            data_paring(file_path)
+            data_paring(file_path, root[-3:])
 
-# 디렉토리 경로 설정 및 호출
-directory_path = '../data/disease_notice/Training/TL_B_반려견'
+directory_path = 'C:/Users/jooho/Desktop/petpoia/data/disease_notice/Training/TL_B_반려견'
+process_directory(directory_path)
+
+directory_path = 'C:/Users/jooho/Desktop/petpoia/data/disease_notice/Training/TL_B_반려묘'
+process_directory(directory_path)
+
+directory_path = 'C:/Users/jooho/Desktop/petpoia/data/disease_notice/Training/02.참조데이터_C_반려견'
 process_directory(directory_path)
--- pet_disease/test.py
+++ pet_disease/test.py
@@ -1,6 +1,64 @@
-import csv
+import numpy as np
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 
-path = '../data/disease_notice/'
-test_file = csv.writer(path)
+def pet_disease_recommend(species, breed, age, pet_class, sex, weight, exercise,
+                          environment, defecation, food_count, food_amount,snack_amount, food_kind,):
+    #1~10
+    weight_breed = 8
+    weight_age = 7
+    weight_pet_class = 7
+    weight_sex = 7
+    weight_weight = 3
+    weight_exercise = 8
+    weight_environment = 7
+    weight_defecation = 9
+    weight_food_count = 3
+    weight_food_amount = 3
+    weight_snack_amount = 5
+    weight_food_kind = 5
 
-test_file.writerow(['me'])
+    pet_features = np.array([breed, age, pet_class, sex, weight, exercise, environment,
+                            defecation, food_count, food_amount, snack_amount, food_kind])
+    pet_features_weight = pet_features * np.array([weight_breed, weight_age, weight_pet_class, weight_sex,
+                                                   weight_weight, weight_exercise, weight_environment, weight_defecation,
+                                                   weight_food_count, weight_food_amount, weight_snack_amount, weight_food_kind])
+
+    #10:dog, 20:cat
+    if species == "10":
+        anonymous_data = pd.read_csv('data/dog_data.csv')
+    elif species == "20":
+        anonymous_data = pd.read_csv('data/cat_data.csv')
+
+    similarity_scores = cosine_similarity([pet_features_weight], anonymous_data['species', 'breed', 'age', 'pet_class', 'sex', 'weight', 'exercise', 'environment', 'defecation', 'food_count', 'food_amount', 'snack_amount', 'food_kind'])
+    most_similar_pet_index = np.argmax(similarity_scores)
+
+    print(similarity_scores)
+    print(most_similar_pet_index)
+
+#데이터 타입 확인
+pet_disease_recommend(10,"BEA",2,"SH","IM",10.4,1,2,1,1,4,0,1)
+
+# def similar_recommend(level, category):
+#
+#   weight_level = 2
+#   weight_category = 5
+#
+#   user_features = np.array([level, category])
+#   user_features_weighted = user_features * np.array([weight_level, weight_category])
+#
+#   anonymous_data = pd.read_csv('C:\project\project\SECodeVerse_API\output.csv')  # 여기에 익명의 사용자들이 푼 문제 데이터가 들어있는 파일 경로를 넣어주세요
+#
+#   similarity_scores = cosine_similarity([user_features_weighted], anonymous_data[['level', 'inserted_pk']])
+#   most_similar_problem_index = np.argmax(similarity_scores)
+#
+#   recommended_problems = []
+#   for i in range(5):
+#       while (
+#           anonymous_data.iloc[most_similar_problem_index]['question_pk'] in recommended_problems
+#       ):
+#           similarity_scores[0, most_similar_problem_index] = -1  # 이미 추천된 문제나 사용자가 푼 문제는 제외
+#           most_similar_problem_index = np.argmax(similarity_scores)
+#       recommended_problems.append(anonymous_data.iloc[most_similar_problem_index]['question_pk'])
+#
+#   return recommended_problems
