7285e3ec3cac769a3ede4c841db51c75870a7801
yurim <117262012+YuRimBang@users.noreply.github.com>
2024-04-18 14:15:41 +0900

    인코딩 중

--- pettopia-AI/Model/pet_disease/Preprocessing/Pet_Data_Encoding.py
+++ pettopia-AI/Model/pet_disease/Preprocessing/Pet_Data_Encoding.py
@@ -1,6 +1,6 @@
 import csv
+from ..Enum import Dog_Breed, Cat_Breed, Pet_Class, Sex, Exercise, Environment, Defecation, Food_Count
 from ..Entity import Pet_Disease_Data
-from ..Enum import Dog_Breed, Cat_Breed, Pet_Class, Sex
 
 class Pet_Data_Encoding():
 
@@ -16,24 +16,23 @@ class Pet_Data_Encoding():
         self.__cat_breed_dictionary = Cat_Breed.Cat_Breed
         self.__pet_class_dictionary = Pet_Class.Pet_Class
         self.__sex_dictionary = Sex.Sex
+        self.__exercise_dictionary = Exercise.Exercise
+        self.__environment_dictionary = Environment.Environment
+        self.__defecation_dictionary = Defecation.Defecation
+        self.__food_count_dictionary = Food_Count.Food_Count
 
-    def data_encoding(self, data:Pet_Disease_Data):
+    def data_encoding(self, data:Pet_Disease_Data.Pet_Disease_Data):
         result={}
 
-        species = 0
-        breed = self.__
-        pet_class = 0
-        sex = 0
-        exercise = 0
-        environment = 0
-        defecation = 0
-
-        if data.get_species() == "반려견":
-            species = 10
-        elif data.get_species() == "반려묘":
-            species = 20
-
-        breed =
+        species = self.species_encoding(data.get_species())
+        breed = self.breed_encoding(data.get_breed(), species)
+        pet_class = self.pet_class_encoding(data.get_pet_class())
+        sex = self.pet_class_encoding(data.get_pet_class())
+        exercise = self.exercise_encoding(data.get_exercise())
+        environment = self.environment_encoding(data.get_environment())
+        defecation = self.defecation_encoding(data.get_defecation())
+        food_count = self.food_count_encoding(data.get_food_count())
+        
 
     def species_encoding(self, data):
         species = 0
@@ -43,15 +42,59 @@ class Pet_Data_Encoding():
         elif(data == "고양이"):
             species = 20
         else:
-            species = 10
+            species = -1
 
         return species
 
     #Todo 각 데이터 인코딩
     def breed_encoding(self, data, species):
         breed = 0
+        print(data)
+        print(species)
 
-        if
+        if species == 10:
+            breed = self.__dog_breed_dictionary.__getitem__(data).value
+        elif species == 20:
+            breed = self.__cat_breed_dictionary.__getitem__(data).value
+        else:
+            breed = -1
+
+        return breed
+
+    def pet_class_encoding(self, data):
+        pet_class = self.__pet_class_dictionary.__getitem__(data).value;
+
+        return pet_class
+
+    def sex_encoding(self, data):
+        sex = self.__sex_dictionary.__getitem__(data).value
+
+        return sex
+
+    def exercise_encoding(self, data):
+        exercise = self.__exercise_dictionary.__getitem__(data).value
+
+        return exercise
+
+    def environment_encoding(self, data):
+        environment = self.__environment_dictionary.__getitem__(data).value
+
+        return  environment
+
+    def defecation_encoding(self, data):
+        defecation = self.__defecation_dictionary.__getitem__(data).value
+
+        return defecation
+
+    def food_count_encoding(self, data):
+        food_count = 0
+
+        if data >= 4 :
+            food_count = self.__food_count_dictionary.__getitem__("FREE").value
+        else:
+            food_count = self.__food_count_dictionary.__getitem__(data).value
+
+        return food_count
 
     def dog_data_encoding_file(self):
         file = open(self.__dog_data_file, 'r')
@@ -86,3 +129,7 @@ class Pet_Data_Encoding():
             with open(self.__cat_encoding_file, 'a', newline='', encoding='UTF8') as f:
                 csv_writer = csv.writer(f)
                 csv_writer.writerow(line)
+
+test = Pet_Data_Encoding()
+t = test.breed_encoding("BEA", 10)
+print(t)
