c0909aa7d2fe5cecf0748d93d098ee0c6cd12313
yurim <117262012+YuRimBang@users.noreply.github.com>
2024-04-19 23:15:18 +0900

    질병알리미 추천 완료/Todo.디코딩
    디코딩 중

---
+++ pettopia-AI/Model/pet_disease/Enum/Disease_Name.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+class Disease_Name(Enum):
+    DER = "피부"
+    MUS = "근골격계"
+    NEU = "신경계"
+    OCU = "안과"
+    RES = "호흡계통"
+    CAR = "심혈계통"
+    HEM = "혈액"
+    GAS = "소화계통"
+    URI = "비뇨계통"
+    REP = "생식계통"
+    END = "내분비계통"
+    INF = "감염성및기생충성"
+    ETC = "기타"
