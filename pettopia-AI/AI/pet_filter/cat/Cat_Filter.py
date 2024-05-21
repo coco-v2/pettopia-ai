9dd6336c09e41cb4cd94243feb4144b762fa3d43
yurim <117262012+YuRimBang@users.noreply.github.com>
2024-05-21 22:46:34 +0900

    .

--- pettopia-AI/AI/pet_filter/cat/Cat_Filter.py
+++ pettopia-AI/AI/pet_filter/cat/Cat_Filter.py
@@ -1,21 +1,23 @@
-import dlib, cv2, os, datetime
-from imutils import face_utils
+import cv2, os, datetime,sys
+sys.path.append('pettopia-AI')
 from math import atan2, degrees
-pwd = os.path.dirname(__file__)
-import matplotlib.pyplot as plt
 import numpy as np
+import tensorflow as tf
+from tensorflow.python.keras.models import load_model
 
 class Cat_Filter():
     def __init__(self):
-        self.__detector = dlib.cnn_face_detection_model_v1(pwd+'/model/dogHeadDetector.dat')
-        self.__predictor = dlib.shape_predictor(pwd+'/model/landmarkDetector.dat')
+
+        detector_model_path = 'C:/Users/jooho/Documents/GitHub/pettopia-ai/pettopia-AI/AI/pet_filter/cat/cat_detector_model.h5'
+        predictor_model_path = 'C:/Users/jooho/Documents/GitHub/pettopia-ai/pettopia-AI/AI/pet_filter/cat/cat_landmark_detector_model.h5'
+
+        # 모델 로드
+        self.__detector = load_model(detector_model_path)
+        self.__predictor = load_model(predictor_model_path)
         self.img = None
         self.img_result = None
         self.file_result = None
-        self.dets = None
-        self.faces = None
-        self.shapes = []
-        self.shape = None
+        self.img_size = 224
 
     def img_read(self, img_path):
         print(img_path)
@@ -27,84 +29,91 @@ class Cat_Filter():
         cv2.imshow("image", img)
         cv2.waitKey(0)
 
+    def resize_img(self, img):
+        old_size = img.shape[:2]  # old_size is in (height, width) format
+        ratio = float(self.img_size) / max(old_size)
+        new_size = tuple([int(x * ratio) for x in old_size])
+        # new_size should be in (width, height) format
+        im = cv2.resize(img, (new_size[1], new_size[0]))
+        delta_w = self.img_size - new_size[1]
+        delta_h = self.img_size - new_size[0]
+        top, bottom = delta_h // 2, delta_h - (delta_h // 2)
+        left, right = delta_w // 2, delta_w - (delta_w // 2)
+        new_im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT,
+                                    value=[0, 0, 0])
+        return new_im, ratio, top, left
+
     def detector_face(self):
-        # 객체 탐지기 사용
-        self.dets = self.__detector(self.img, upsample_num_times=1)
 
-        img_result = self.img.copy()
+        self.img_result, ratio, top, left = self.resize_img(self.img)
+        inputs = (self.img_result.astype('float32') / 255).reshape((1, self.img_size, self.img_size, 3))
+        pred_bb = self.__detector.predict(inputs)[0].reshape((-1, 2))
 
-        for i, d in enumerate(self.dets):
-            print("Detection {}: Left: {} Top: {} Right: {} Bottom: {} Confidence: {}".format(i, d.rect.left(),
-                                                                                              d.rect.top(),
-                                                                                              d.rect.right(),
-                                                                                              d.rect.bottom(),
-                                                                                              d.confidence))
+        ori_bb = ((pred_bb - np.array([left, top])) / ratio).astype(np.int32)
 
-            x1, y1 = d.rect.left(), d.rect.top()
-            x2, y2 = d.rect.right(), d.rect.bottom()
+        return ori_bb
 
-            cv2.rectangle(img_result, pt1=(x1, y1), pt2=(x2, y2), thickness=2, color=(255, 0, 0), lineType=cv2.LINE_AA)
+    def detector_landmarks(self, ori_bb):
+        center = np.mean(ori_bb, axis=0)
+        face_size = max(np.abs(ori_bb[1] - ori_bb[0]))
+        new_bb = np.array([
+            center - face_size * 0.6,
+            center + face_size * 0.6
+        ])
+        new_bb = np.clip(new_bb, 0, 99999)
 
-        self.img_result = img_result
+        # 랜드마크 예측
+        face_img = self.img_result[new_bb[0][1]:new_bb[1][1], new_bb[0][0]:new_bb[1][0]]
 
-    def detector_landmarks(self):
+        face_img, face_ratio, face_top, face_left = self.resize_img(face_img)
 
-        for i, d in enumerate(self.dets):
-            self.shape = self.__predictor(self.img, d.rect)
-            self.shape = face_utils.shape_to_np(self.shape)
+        face_inputs = (face_img.astype('float32') / 255).reshape((1, self.img_size, self.img_size, 3))
 
-            for i, p in enumerate(self.shape):
-                self.shapes.append(self.shape)
-                cv2.circle(self.img_result, center=tuple(p), radius=3, color=(0, 0, 255), thickness=-1, lineType=cv2.LINE_AA)
-                cv2.putText(self.img_result, str(i), tuple(p), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1,
-                            cv2.LINE_AA)
+        pred_lmks = self.__predictor.predict(face_inputs)[0].reshape((-1, 2))
 
-    def dog_filter(self, filter_head, filter_nose):
+        # 원래 이미지에서 랜드마크 계산
+        new_lmks = ((pred_lmks - np.array([face_left, face_top])) / face_ratio).astype(np.int)
+        ori_lmks = new_lmks + new_bb[0]
+
+        return ori_lmks
+
+    def cat_filter(self, filter, ori_lmks):
+        glasses = cv2.imread(filter, cv2.IMREAD_UNCHANGED)
 
         img_result2 = self.img.copy()
+        glasses_center = np.mean([ori_lmks[0], ori_lmks[1]], axis=0)
+        glasses_size = np.linalg.norm(ori_lmks[0] - ori_lmks[1]) * 2
 
-        horns = cv2.imread(filter_head, cv2.IMREAD_UNCHANGED)
-        horns_h, horns_w = horns.shape[:2]
+        angle = -self.angle_between(ori_lmks[0], ori_lmks[1])
+        M = cv2.getRotationMatrix2D((glasses_center.shape[1] / 2, glasses.shape[0] / 2), angle, 1)
+        rotated_glasses = cv2.warpAffine(glasses, M, (glasses.shape[1], glasses.shape[0]))
 
-        nose = cv2.imread(filter_nose, cv2.IMREAD_UNCHANGED)
-
-        for shape in self.shapes:
-            horns_center = np.mean([shape[4], shape[1]], axis=0) // [1, 1.05]
-            horns_size = np.linalg.norm(shape[4] - shape[1]) * 2
-
-            nose_center = shape[3]
-            nose_size = horns_size // 4
-
-            angle = -self.angle_between(shape[4], shape[1])
-            M = cv2.getRotationMatrix2D((horns_w, horns_h), angle, 1)
-            rotated_horns = cv2.warpAffine(horns, M, (horns_w, horns_h))
-
-            img_result2 = self.overlay_transparent(img_result2, nose, nose_center[0], nose_center[1],
-                                              overlay_size=(int(nose_size), int(nose_size)))
-            try:
-                img_result2 = self.overlay_transparent(img_result2, rotated_horns, horns_center[0], horns_center[1],
-                                                  overlay_size=(int(horns_size), int(horns_h * horns_size / horns_w)))
-            except:
-                print('failed overlay image')
+        try:
+            result_img = self.overlay_transparent(self.img, rotated_glasses, glasses_center[0], glasses_center[1],
+                                             overlay_size=(int(glasses_size),
+                                                           int(glasses.shape[0] * glasses_size / glasses.shape[1])))
+        except:
+            print('failed overlay image')
 
         current_datetime = datetime.datetime.now()
 
         filename = current_datetime.strftime("%Y%m%d_%H%M%S")  # 예: 20220425_164230
 
-        image_path = 'C:/Users/jooho/Documents/GitHub/pettopia-ai/pettopia-AI/AI/pet_filter/dog/image/res_img/' + filename + '.jpg'
+        image_path = 'C:/Users/jooho/Documents/GitHub/pettopia-ai/pettopia-AI/AI/pet_filter/cat/image/res_img/' + filename + '.jpg'
 
         # if not cv2.imwrite(image_path, cv2.cvtColor(img_result2, cv2.COLOR_BGRA2BGR)):
         #     print("사진 저장 실패")
         # else:
         #     print(f"사진 저장 성공: {image_path}")
 
-        self.img_result = img_result2
+        self.img_result = result_img
         self.file_result = filename
 
     def overlay_transparent(self, background_img, img_to_overlay_t, x, y, overlay_size=None):
-        bg_img = background_img
+        bg_img = background_img.copy()
+
         if bg_img.shape[2] == 3:
-            bg_img = cv2.cvtColor(bg_img, cv2.COLOR_BGR2BGRA)  # RGB를 BGRA로 변환
+            bg_img = cv2.cvtColor(bg_img, cv2.COLOR_BGR2BGRA)
 
         if overlay_size is not None:
             img_to_overlay_t = cv2.resize(img_to_overlay_t.copy(), overlay_size)
@@ -121,13 +130,26 @@ class Cat_Filter():
 
         bg_img[int(y - h / 2):int(y + h / 2), int(x - w / 2):int(x + w / 2)] = cv2.add(img1_bg, img2_fg)
 
-        if bg_img.shape[2] == 4:
-            bg_img = cv2.cvtColor(bg_img, cv2.COLOR_BGRA2BGR)  # 결과를 BGRA에서 BGR로 변환, 필요한 경우에만
+        bg_img = cv2.cvtColor(bg_img, cv2.COLOR_BGRA2BGR)
 
         return bg_img
-
     def angle_between(self,p1, p2):
         xDiff = p2[0] - p1[0]
         yDiff = p2[1] - p1[1]
 
         return degrees(atan2(yDiff, xDiff))
+
+mypath = 'image/cat.jpg'
+test = Cat_Filter()
+test.img_read(mypath)
+d = test.detector_face()
+lm = test.detector_landmarks(d)
+test.cat_filter('glasses.png', lm)
+test.img_show_result(test.img_result)
+# mypath = 'image/dog1.jpg'
+# c = Dog_Filter()
+# c.img_read(mypath)
+# c.detector_face()
+# c.detector_landmarks()
+# c.dog_filter("image/horns2.png", "image/nose.png")
+# c.img_show_result(c.img_result)
